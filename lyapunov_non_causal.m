4a69892605f0b64519524b64cd29f0517d08ad7d
tukai <snc.jobs@outlook.com>
2016-07-12 10:09:09 +0200

    v2.2

--- lyapunov_non_causal.m
+++ lyapunov_non_causal.m
@@ -1,7 +1,18 @@
 function[sum_cap_avg]=lyapunov_non_causal(n,m,a,theta,tau_l,T,g,h,P_max,b_m,p_s,Eng_h,Q_avg,nH)
 
+%---------------------------------------
+% The Throughput Optimization Problem
+% Infinite Horizon
+% Non-Causal CSI and ESI
+% Energy Harvesting
 
+% Noise Variance
+n_var=1;
 
+% Interference Variance
+inf_var=2;
 
+% Probability of Spectrum Efficiency
+mu=0.8;
 
 end
